ab58c79d246686d08e6fdcab5d710698071c54c5
Adityadagda3 <91243443+Adityadagda3@users.noreply.github.com>
2022-10-09 09:24:03 +0530

    Add files via upload

---
+++ AboutUs/action_page.php
@@ -0,0 +1,9 @@
+<?php
+    $firstname = $_POST['firstname']
+    $lasttname = $_POST['lastname']
+    $country = $_POST['country']
+    $state = $_POST['state']
+    $subject = $_POST['subject']
+    
+
+?>
